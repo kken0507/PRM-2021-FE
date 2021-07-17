22fdf907caf7a24f44b5dab9dcd688d7498599ce
KienNT <knguyen.05.07.97@gmail.com>
2021-07-18 02:34:38 +0700

    non-create customer

--- restaurant-order-system/lib/screens/account_management/details/detail_form.dart
+++ restaurant-order-system/lib/screens/account_management/details/detail_form.dart
@@ -100,46 +100,48 @@ class _DetailFormState extends State<DetailForm> {
               //     ),
               //   ),
               // ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: FormField<String>(
-                  builder: (FormFieldState<String> state) {
-                    return InputDecorator(
-                      decoration: InputDecoration(
-                        // labelStyle: TextStyle(
-                        //     color: Colors.redAccent, fontSize: 20.0),
-                        errorStyle:
-                            TextStyle(color: Colors.redAccent, fontSize: 16.0),
-                        labelText: 'Gender:',
-                        hintText: 'Please select...',
-                        // border: OutlineInputBorder(
-                        //     borderRadius: BorderRadius.circular(5.0))
-                        border: UnderlineInputBorder(),
-                      ),
-                      isEmpty: _currentSelectedGender == '',
-                      child: DropdownButtonHideUnderline(
-                        child: DropdownButton<String>(
-                          value: _currentSelectedGender,
-                          isDense: true,
-                          onChanged: (String newValue) {
-                            setState(() {
-                              _currentSelectedGender = newValue;
-                              widget.genderController.text = newValue;
-                              state.didChange(newValue);
-                            });
-                          },
-                          items: _genders.map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
+              if (_currentSelectedRole !=
+                  MY_ROLES.CUSTOMER.toString().split(".").last)
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: FormField<String>(
+                    builder: (FormFieldState<String> state) {
+                      return InputDecorator(
+                        decoration: InputDecoration(
+                          // labelStyle: TextStyle(
+                          //     color: Colors.redAccent, fontSize: 20.0),
+                          errorStyle: TextStyle(
+                              color: Colors.redAccent, fontSize: 16.0),
+                          labelText: 'Gender:',
+                          hintText: 'Please select...',
+                          // border: OutlineInputBorder(
+                          //     borderRadius: BorderRadius.circular(5.0))
+                          border: UnderlineInputBorder(),
                         ),
-                      ),
-                    );
-                  },
+                        isEmpty: _currentSelectedGender == '',
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<String>(
+                            value: _currentSelectedGender,
+                            isDense: true,
+                            onChanged: (String newValue) {
+                              setState(() {
+                                _currentSelectedGender = newValue;
+                                widget.genderController.text = newValue;
+                                state.didChange(newValue);
+                              });
+                            },
+                            items: _genders.map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
                 child: TextFormField(
@@ -164,36 +166,39 @@ class _DetailFormState extends State<DetailForm> {
                   ),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  validator: (value) {
-                    Pattern pattern = r'^(?:[+0]9)?[0-9]{10}$';
-                    // Pattern pattern =
-                    //     r'/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im';
-                    RegExp regex = new RegExp(pattern);
-                    if (value.isEmpty) {
-                      return "Empty value";
-                    }
-                    if (value.length != 10) {
-                      return "Phone number must be 10 digits";
-                    }
-                    if (!regex.hasMatch(value)) {
-                      return 'Invalid Format';
-                    }
-                    return null;
-                  },
-                  controller: widget.phoneController,
-                  maxLines: null,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Phone:',
+              if (_currentSelectedRole !=
+                  MY_ROLES.CUSTOMER.toString().split(".").last)
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    validator: (value) {
+                      Pattern pattern = r'^(?:[+0]9)?[0-9]{10}$';
+                      // Pattern pattern =
+                      //     r'/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im';
+                      RegExp regex = new RegExp(pattern);
+                      if (value.isEmpty) {
+                        return "Empty value";
+                      }
+                      if (value.length != 10) {
+                        return "Phone number must be 10 digits";
+                      }
+                      if (!regex.hasMatch(value)) {
+                        return 'Invalid Format';
+                      }
+                      return null;
+                    },
+                    controller: widget.phoneController,
+                    maxLines: null,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Phone:',
+                    ),
                   ),
                 ),
-              ),
-              if (!(_currentSelectedRole !=
-                  MY_ROLES.MANAGER.toString().split('.').last))
+              // if (!(_currentSelectedRole !=
+              //     MY_ROLES.MANAGER.toString().split('.').last && _currentSelectedRole !=
+              //     MY_ROLES.CUSTOMER.toString().split('.').last))
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
                   child: TextFormField(
@@ -204,85 +209,89 @@ class _DetailFormState extends State<DetailForm> {
                       return null;
                     },
                     controller: widget.roleController,
-                    readOnly: widget.roleController.text ==
-                        MY_ROLES.MANAGER.toString().split('.').last,
+                    // readOnly: widget.roleController.text ==
+                    // MY_ROLES.MANAGER.toString().split('.').last,
+                    readOnly: true,
                     decoration: InputDecoration(
                       border: UnderlineInputBorder(),
                       labelText: 'Role:',
                     ),
                   ),
                 ),
+              // if (_currentSelectedRole !=
+              //     MY_ROLES.MANAGER.toString().split('.').last && _currentSelectedRole !=
+              //     MY_ROLES.CUSTOMER.toString().split('.').last)
+              //   Padding(
+              //     padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              //     child: FormField<String>(
+              //       builder: (FormFieldState<String> state) {
+              //         return InputDecorator(
+              //           decoration: InputDecoration(
+              //             // labelStyle: TextStyle(
+              //             //     color: Colors.redAccent, fontSize: 20.0),
+              //             errorStyle: TextStyle(
+              //                 color: Colors.redAccent, fontSize: 16.0),
+              //             labelText: 'Role:',
+              //             hintText: 'Please select...',
+              //             // border: OutlineInputBorder(
+              //             //     borderRadius: BorderRadius.circular(5.0))
+              //             border: UnderlineInputBorder(),
+              //           ),
+              //           isEmpty: _currentSelectedRole == '',
+              //           child: DropdownButtonHideUnderline(
+              //             child: DropdownButton<String>(
+              //               value: _currentSelectedRole,
+              //               isDense: true,
+              //               onChanged: (String newValue) {
+              //                 setState(() {
+              //                   _currentSelectedRole = newValue;
+              //                   widget.roleController.text = newValue;
+              //                   state.didChange(newValue);
+              //                 });
+              //               },
+              //               items: _roles.map((String value) {
+              //                 return DropdownMenuItem<String>(
+              //                   value: value,
+              //                   child: Text(value),
+              //                 );
+              //               }).toList(),
+              //             ),
+              //           ),
+              //         );
+              //       },
+              //     ),
+              //   ),
               if (_currentSelectedRole !=
-                  MY_ROLES.MANAGER.toString().split('.').last)
+                  MY_ROLES.CUSTOMER.toString().split(".").last)
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: FormField<String>(
-                    builder: (FormFieldState<String> state) {
-                      return InputDecorator(
-                        decoration: InputDecoration(
-                          // labelStyle: TextStyle(
-                          //     color: Colors.redAccent, fontSize: 20.0),
-                          errorStyle: TextStyle(
-                              color: Colors.redAccent, fontSize: 16.0),
-                          labelText: 'Role:',
-                          hintText: 'Please select...',
-                          // border: OutlineInputBorder(
-                          //     borderRadius: BorderRadius.circular(5.0))
-                          border: UnderlineInputBorder(),
-                        ),
-                        isEmpty: _currentSelectedRole == '',
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton<String>(
-                            value: _currentSelectedRole,
-                            isDense: true,
-                            onChanged: (String newValue) {
-                              setState(() {
-                                _currentSelectedRole = newValue;
-                                widget.roleController.text = newValue;
-                                state.didChange(newValue);
-                              });
-                            },
-                            items: _roles.map((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      );
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Empty value";
+                      }
+                      if (DateTime.parse(value).isAfter(DateTime.now())) {
+                        return "Can not set future date";
+                      }
+                      if (DateTime.now()
+                              .difference(DateTime.parse(value))
+                              .inDays <=
+                          6570) {
+                        return "Age must over 18";
+                      }
+                      if (!MyUtil().isValidDate(value)) {
+                        return "Value not valid";
+                      }
+                      return null;
                     },
+                    keyboardType: TextInputType.datetime,
+                    controller: widget.dobController,
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Date of birth:',
+                    ),
                   ),
                 ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return "Empty value";
-                    }
-                    if (DateTime.parse(value).isAfter(DateTime.now())) {
-                      return "Can not set future date";
-                    }
-                    if (DateTime.now()
-                            .difference(DateTime.parse(value))
-                            .inDays <=
-                        6570) {
-                      return "Age must over 18";
-                    }
-                    if (!MyUtil().isValidDate(value)) {
-                      return "Value not valid";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.datetime,
-                  controller: widget.dobController,
-                  decoration: InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Date of birth:',
-                  ),
-                ),
-              ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
                 child: CheckboxListTile(
